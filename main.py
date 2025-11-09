dd6ff318413d264fd99ce423446785cc7f6bd1c5
unknown <abululb02@gmail.com>
2025-11-09 19:11:53 +0000

    Get Prayer Times

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 from bs4 import BeautifulSoup
+import requests
 
+response = requests.get("https://www.leedsgrandmosque.com/")
+soup = BeautifulSoup(response.text, "html.parser")
+
+prayer_times = {}
+prayer_list = soup.find(class_="prayers-list").find_all("li")
+for prayer in prayer_list:
+    name = prayer.find(class_="prayer-name").text.capitalize()
+    time = prayer.find(class_="date").text
+    prayer_times[name] = time
+
+print(prayer_times)
